bfcdc5b3d3cf9effbf34d68ebb5fc4bf2bfa7291
Danex2 <khadane.miller@gmail.com>
2019-09-16 13:47:33 -0400

    :poop: added a nice lil message

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const date = document.getElementById('date');
 const time = document.getElementById('time');
+const message = document.getElementById('day-message');
 
 date.innerHTML = new Date().toLocaleDateString('en-US', {
   year: '2-digit',
@@ -7,11 +8,22 @@ date.innerHTML = new Date().toLocaleDateString('en-US', {
   day: '2-digit'
 });
 
-setInterval(
-  () =>
-    (time.innerHTML = new Date().toLocaleTimeString('en-US', {
-      hour12: true,
-      hour: 'numeric',
-      minute: 'numeric'
-    }))
-);
+setInterval(() => {
+  time.innerHTML = new Date().toLocaleTimeString('en-US', {
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  });
+}, 1000);
+
+const hour = new Date().getHours();
+
+setInterval(() => {
+  if (hour >= 12 && hour <= 17) {
+    message.innerHTML = 'Good afternoon.';
+  } else if (hour >= 18 && hour <= 23) {
+    message.innerHTML = 'Good night.';
+  } else {
+    message.innerHTML = 'Good morning.';
+  }
+}, 60 * 60 * 1000);
